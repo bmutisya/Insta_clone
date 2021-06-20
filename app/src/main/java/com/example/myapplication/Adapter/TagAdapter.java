fd239fbb1b1057d1601576bf370f07bfc3a82cb6
bmutisya <benmutisya35@gmail.com>
2021-06-20 22:05:44 +0300

    TagAdapter

---
+++ app/src/main/java/com/example/myapplication/Adapter/TagAdapter.java
@@ -0,0 +1,68 @@
+package com.example.myapplication.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class TagAdapter extends RecyclerView.Adapter<TagAdapter.viewHolder> {
+    private Context mContext;
+    private List<String> mTags;
+    private List<String> mTagsCount;
+
+
+    public TagAdapter(Context mContext, List<String> mTags, List<String> mTagsCount) {
+        this.mContext = mContext;
+        this.mTags = mTags;
+        this.mTagsCount = mTagsCount;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view =LayoutInflater.from(mContext).inflate(R.layout.tag_item,parent,false);
+
+        return new TagAdapter.viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull TagAdapter.viewHolder holder, int position) {
+       //holder.tag.setText("#" + mTags.get(position));
+        holder.noOfPosts.setText(mTagsCount.get(position)+ "posts");
+
+    }
+
+    @Override
+    public int getItemCount() {
+                return mTags.size();
+    }
+
+    public  class viewHolder extends RecyclerView.ViewHolder{
+        public TextView tag;
+        public TextView noOfPosts;
+
+        public viewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            tag = itemView.findViewWithTag(R.id.hash_tag);
+            noOfPosts=itemView.findViewById(R.id.no_of_posts);
+
+        }
+    }
+   // public void filter (List<String> filterTags,List<String> filterTagsCount ){
+
+        //this.mTags =filterTags;
+        //.mTagsCount= filterTagsCount;
+       // notifyDataSetChanged();
+   // }
+}
--- app/src/main/java/com/example/myapplication/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/myapplication/Fragments/SearchFragment.java
@@ -7,11 +7,15 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.myapplication.Adapter.TagAdapter;
 import com.example.myapplication.Adapter.UserAdapter;
 import com.example.myapplication.Model.User;
 import com.example.myapplication.R;
@@ -19,6 +23,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
 
@@ -34,37 +39,91 @@ public class SearchFragment extends Fragment {
     private List<User> mUsers;
     private UserAdapter userAdapter;
 
+    private RecyclerView recyclerViewTags;
+    private List<String> mHashTags;
+    private List<String> mHashTagsCount;
+    private TagAdapter tagAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment_search, container, false);
-        recyclerView=view.findViewById(R.id.recycler_view_user);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        recyclerView = view.findViewById(R.id.recycler_view_user);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        recyclerViewTags = view.findViewById(R.id.recycler_view_tags);
+        recyclerViewTags.setHasFixedSize(true);
+        recyclerViewTags.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        mUsers= new ArrayList<>();
-        userAdapter=new UserAdapter(getContext(), mUsers,true);
+        mHashTags = new ArrayList<>();
+        mHashTagsCount = new ArrayList<>();
+        tagAdapter = new TagAdapter(getContext(), mHashTags, mHashTagsCount);
+        recyclerViewTags.setAdapter(tagAdapter);
+
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext(), mUsers, true);
         recyclerView.setAdapter(userAdapter);
 
-        search_bar=view.findViewById(R.id.search_bar);
+        search_bar = view.findViewById(R.id.search_bar);
 
         readUsers();
+        readTags();//method to read tags available
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //filter(s.toString());
+
+            }
+        });
 
 
         return view;
     }
 
+    private void readTags() {
+        FirebaseDatabase.getInstance().getReference().child("HashTags").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                mHashTags.clear();
+                mHashTagsCount.clear();
+                for (DataSnapshot snapsshot : snapshot.getChildren()) {
+                    mHashTags.add(snapsshot.getKey());
+                    mHashTagsCount.add(snapsshot.getChildrenCount() + "");
+
+                }
+                tagAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+    }
+
     private void readUsers() {
-        DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("users");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users");
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                if (TextUtils.isEmpty(search_bar.getText().toString())){
+                if (TextUtils.isEmpty(search_bar.getText().toString())) {
                     mUsers.clear();
-                    for (DataSnapshot snapshott :snapshot.getChildren()){
-                        User user= snapshott.getValue(User.class);
+                    for (DataSnapshot snapshott : snapshot.getChildren()) {
+                        User user = snapshott.getValue(User.class);
                         mUsers.add(user);
                     }
                     userAdapter.notifyDataSetChanged();
@@ -77,4 +136,38 @@ public class SearchFragment extends Fragment {
             }
         });
     }
+
+    private void searchUser(String s) {
+        Query query = FirebaseDatabase.getInstance().getReference().child("users").
+                orderByChild("username").startAt(s).endAt(s + "\uf8ff");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                mUsers.clear();
+                for (DataSnapshot snapshott : snapshot.getChildren()) {
+                    User user = snapshott.getValue(User.class);
+                    mUsers.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+    }
+
+   /// private void filter(String text) {
+     //   List<String> mSearchTags = new ArrayList<>();
+      //  List<String> mSearchTagsCount = new ArrayList<>();
+      //  for (String s : mHashTags) {
+       //     if (s.toLowerCase().contains(text.toLowerCase())) {
+              //  mSearchTags.add(s);
+              //  mSearchTagsCount.add(mSearchTagsCount.get(mHashTags.indexOf(s)));
+          //  }
+      //  }
+        //tagAdapter.filter(mSearchTags,mSearchTagsCount);
+
+    //}
 }
